c46dd6c850e8829d7bfa05eac8837461837e05ab
YanHin <79831087+yhlee10@users.noreply.github.com>
2022-12-15 00:49:04 +0800

    Update Location.js

--- src/Components/Location.js
+++ src/Components/Location.js
@@ -9,20 +9,6 @@ class Location extends Component {
         }
     }
 
-    // const fs = require('fs');
-    // const https = require('https');
-    // const xml2js = require('xml2js');
-
-
-    // var file_dir_xml = __dirname + '/XMLfile/';
-    // app.post('/getXML', (req, res) => {
-    //     var url = 'https://www.lcsd.gov.hk/datagovhk/event/events.xml';
-    //     var file_name = 'events.xml';
-    //     var file = fs.createWriteStream(file_dir_xml + file_name, {'flags': 'w'});
-    //     const get = https.get(url, (response) => {
-    //         response.pipe(file);
-    //     });
-    // });
     componentDidMount() {//load for each set state(just want once=> new class + new componentdidmount)
         axios({
             // need change localhost to the publicIP
